f61f891e173c60e88cf1349e89f00bd7a1c94999
Carlton Gibson <carlton.gibson@noumenal.es>
2026-09-11 12:30:00 +0200

    Read sensitive variables from environment.
    DEBUG, ALLOWED_HOSTS, and SECRET_KEY to begin.

--- src/auction/settings.py
+++ src/auction/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9cgzc7v4^#6s!!*zmgfyt4+8+pr==#)+n$ar6wo+m2ll(p6bsz"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "django-insecure-9cgzc7v4^#6s!!*zmgfyt4+8+pr==#)+n$ar6wo+m2ll(p6bsz"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "true").lower() in ("1", "true", "yes")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [h for h in os.environ.get("ALLOWED_HOSTS", "").split(",") if h]
 
 
 # Application definition
